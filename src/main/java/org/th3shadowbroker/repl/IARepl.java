6c495f9551d15d1618595916764d9a9720ce1675
Th3Shadowbroker <webmaster@m4taiori.de>
2022-11-22 11:53:11 +0100

    Initial commit

---
+++ src/main/java/org/th3shadowbroker/repl/IARepl.java
@@ -0,0 +1,25 @@
+package org.th3shadowbroker.repl;
+
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class IARepl extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        // Change type of block
+        event.getBlock().setType(Material.AIR);
+
+        // Cancel original event
+        event.setCancelled(true);
+    }
+
+}
